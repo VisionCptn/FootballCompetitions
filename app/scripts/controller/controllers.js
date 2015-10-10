9934fc88f4455ac2394b50d6620424a353a8bf07
yura.hulpa <hulpa.yura@gmail.com>
2015-10-10 23:17:19 +0200

    cleanup.

--- app/scripts/controller/controllers.js
+++ app/scripts/controller/controllers.js
@@ -1,32 +1,10 @@
-"use strict"
+"use strict";
 
 var myApp = angular.module("competition", []);
 
 myApp.controller("retrieveCompetitions", function ($scope, $http, $q) {
     $scope.name = "world";
 
-    //// get available championships
-    //$http({
-    //    method: "get",
-    //    url: "http://footballbet.com.ua/api/championships/"
-    //}).then(function successCallback(response) {
-    //    console.log(response.data.result);
-    //    $scope.competitions = response.data.result;
-    //
-    //}, function errorCallback(response){s
-    //
-    //});
-    //
-    //$http({
-    //    method: "get",
-    //    url: "http://footballbet.com.ua/api/teams/"
-    //}).then(function successCallback(response) {
-    //    console.log(response.data.result);
-    //    $scope.teams = response.data.result;
-    //
-    //}, function errorCallback(response){
-    //
-    //});
 
     var getResources = function(urls){
          var resources = urls.map(function(url) {
@@ -41,7 +19,7 @@ myApp.controller("retrieveCompetitions", function ($scope, $http, $q) {
         return $q.all(resources);
     };
 
-    var promises = getResources(["http://footballbet.com.ua/api/teams/", "http://footballbet.com.ua/api/championships/"])
+    var promises = getResources(["http://footballbet.com.ua/api/teams/", "http://footballbet.com.ua/api/championships/"]);
     promises.then(function (data) {
             // data here is 2 arrays that you will need
             console.log(data);
