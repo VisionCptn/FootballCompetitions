624a79767f2b03e71438a4c180ac216d7ce5113e
yura.hulpa <hulpa.yura@gmail.com>
2015-10-10 22:49:04 +0200

    it looks cleaner to use Q instead of multiple callbacks.

--- app/scripts/controller/controllers.js
+++ app/scripts/controller/controllers.js
@@ -2,29 +2,52 @@
 
 var myApp = angular.module("competition", []);
 
-myApp.controller("retrieveCompetitions", function ($scope, $http) {
+myApp.controller("retrieveCompetitions", function ($scope, $http, $q) {
     $scope.name = "world";
 
-    // get available championships
-    $http({
-        method: "get",
-        url: "http://footballbet.com.ua/api/championships/"
-    }).then(function successCallback(response) {
-        console.log(response.data.result);
-        $scope.competitions = response.data.result;
+    //// get available championships
+    //$http({
+    //    method: "get",
+    //    url: "http://footballbet.com.ua/api/championships/"
+    //}).then(function successCallback(response) {
+    //    console.log(response.data.result);
+    //    $scope.competitions = response.data.result;
+    //
+    //}, function errorCallback(response){s
+    //
+    //});
+    //
+    //$http({
+    //    method: "get",
+    //    url: "http://footballbet.com.ua/api/teams/"
+    //}).then(function successCallback(response) {
+    //    console.log(response.data.result);
+    //    $scope.teams = response.data.result;
+    //
+    //}, function errorCallback(response){
+    //
+    //});
 
-    }, function errorCallback(response){
+    var getResources = function(urls){
+         var resources = urls.map(function(url) {
 
-    });
+            return $http({
+                    method: "get",
+                    url: url
+                })
 
-    $http({
-        method: "get",
-        url: "http://footballbet.com.ua/api/teams/"
-    }).then(function successCallback(response) {
-        console.log(response.data.result);
-        $scope.teams = response.data.result;
+        });
 
-    }, function errorCallback(response){
+        return $q.all(resources);
+    };
 
-    });
+    var promises = getResources(["http://footballbet.com.ua/api/teams/", "http://footballbet.com.ua/api/championships/"])
+    promises.then(function (data) {
+            // data here is 2 arrays that you will need
+            console.log(data);
+        }
+    )
+
+
+
 });
